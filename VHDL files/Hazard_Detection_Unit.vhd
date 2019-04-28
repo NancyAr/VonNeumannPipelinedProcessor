21b2de0a2d711b1ed11b7f372f9841871a4e6f48
abeermroshdy <abeerrefay1@gamil.com>
2019-04-28 12:20:31 +0200

    hazard detection unit

---
+++ VHDL files/Hazard_Detection_Unit.vhd
@@ -0,0 +1,25 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.numeric_std.all;
+use IEEE.std_logic_unsigned.all;
+
+entity Hazard_Detection_Unit is
+	
+	port( R_src,R_dst: in std_logic_vector(2 downto 0);
+		mem_Read: in std_logic;
+		PC_write,Stall_signal:out std_logic);
+end entity;
+
+architecture DataFlow of Hazard_Detection_Unit is
+begin
+	process(mem_Read)
+		begin
+		IF( R_src = R_dst and mem_Read = '1') then
+			PC_write<= '1';
+			stall_signal <='1';
+		else 
+			PC_write<= '0';
+			stall_signal <='0';
+		end if;
+	end process;
+end architecture;
